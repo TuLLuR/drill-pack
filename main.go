87d85a685b0fa8eff23d326aed95127eeb647107
TuLLuR <tullur69@mail.ru>
2018-04-12 16:52:23 +0300

    liss curve add

--- main.go
+++ main.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"./src/lissajous"
 )
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
 
+func lissajousCurve(w http.ResponseWriter, r *http.Request) {
+	lissajous.Lissajous(w)
+}
 func main() {
 	http.HandleFunc("/", greet)
+	http.HandleFunc("/lissajous", lissajousCurve)
+	fmt.Println("It Works!")
 	http.ListenAndServe(":8080", nil)
 }
---
+++ src/lissajous/lissajous.go
@@ -0,0 +1,47 @@
+package lissajous
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"time"
+)
+
+// Simple color pallete {white/grey - red}
+var palette = []color.Color{color.RGBA{92, 87, 87, 1}, color.RGBA{92, 3, 3, 1}}
+
+const (
+	firstColorIndenx = 0
+	secondColorIndex = 1
+)
+
+// Lissajous -
+func Lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	rand.Seed(time.Now().UTC().UnixNano())
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), secondColorIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
